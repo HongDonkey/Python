5d2bfa6c8867ca4cbf113c2650f40e94635e17c9
HongDonkey <hdk1992@naver.com>
2021-04-12 20:01:32 +0900

    210412

---
+++ Session03 - 데이터베이스의 이해 및 ETL 실습/etlProcess.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+# In[6]:
+
+
+indata = pd.read_csv("../dataset/kopo_decision_tree_all_new.csv")
+indata.shape
+indata.columns = indata.columns.str.lower()
+indata.columns
+
+
+# In[7]:
+
+
+targetDbIp = "192.168.110.111"
+targetDbPort = "5432"
+targetDbId = "kopo"
+targetDbPw = "kopo"
+targetDbName = "kopodb"
+
+
+# In[8]:
+
+
+targetDbPrefix = "postgresql://"
+
+
+# In[9]:
+
+
+targetUrl = "{0}{1}:{2}@{3}:{4}/{5}".format(targetDbPrefix,
+                                            targetDbId,
+                                            targetDbPw,
+                                            targetDbIp,
+                                            targetDbPort,
+                                            targetDbName)
+
+
+# In[10]:
+
+
+tableName = "pgout_kopo_hdk"
+pg_kopo_engine = create_engine(targetUrl)
+
+
+# In[11]:
+
+
+indata.to_sql(name=tableName,
+             con = pg_kopo_engine,
+             if_exists="replace", index = False)
+
+
+# In[12]:
+
+
+callDB=pd.read_sql(sql = "select * from pgout_kopo_hdk ", con = pg_kopo_engine)
+callDB
+
+
+# In[13]:
+
+
+tableName = "pgout_kopo_hdk"
+try: 
+        indata.to_sql(name=tableName,con = pg_kopo_engine, if_exists="replace",index =False)
+        print("{} unload 성공!".format(tableName))
+except Expection as e:
+        print(e)
+
+
+# In[ ]:
+
+
+
+
